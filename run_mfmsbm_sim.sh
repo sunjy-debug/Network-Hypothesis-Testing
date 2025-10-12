75a85596421b38c3f3b822e32cf43c9982942fdf
sunjy-debug <sunjy025@uw.edu>
2025-10-11 19:30:25 -0700

    Add files via upload

---
+++ run_mfmsbm_sim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=mfmsbm-nsbm
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#SBATCH --output=Output/logs/%x-%j.out
+#SBATCH --error=Output/logs/%x-%j.err
+
+ml purge
+ml fhR/4.4.1-foss-2023b-R-4.4.1
+
+mkdir -p Output/logs
+unset DISPLAY
+
+export OUTDIR="$PWD/Output"
+
+Rscript --vanilla MFMSBM_simultaneous.R
