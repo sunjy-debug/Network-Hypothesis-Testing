fcfaa1a0eaa4481f2f6ae978e3f88b8bc2ccb216
sunjy-debug <sunjy025@uw.edu>
2025-10-23 18:09:30 -0700

    Add files via upload

--- time_consumption.R
+++ time_consumption.R
@@ -1,5 +1,13 @@
 .libPaths("~/R/x86_64-pc-linux-gnu-library/4.4")
 library(rslurm)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(fossil)
+library(invgamma)
+library(MASS)
+library(igraph)
+library(ConjugateDP)
 library(microbenchmark)
 library(noisySBM)
 library(noisysbmGGM)
@@ -22,20 +30,20 @@ data_generation = env_mfmsbmseq$NSBM_generation()
 X = data_generation$X
 tau = c(0.005, 0.025, 0.05, 0.1, 0.15, 0.25)
 mb = microbenchmark(
-  KLLN = {
-    fit_klln = env_klln$main_noisySBM_fit(X, NIG = TRUE)
-    infer_klln = lapply(tau, function(x) env_klln$main_noisySBM_infer(fit_klln$Rho, fit_klln$theta, fit_klln$Z, X, alpha = x)$A)
-    invisible(infer_klln)
+  MFMSBM_seq = {
+    fit_MFM = env_mfmsbmseq$MFMSBM_estimation(X, niterations = 300, delta = 2, xi = .5, rou = 2.5, kappa = 1, alpha = 4, beta = 7, gamma = 10, lambda = 2)
+    infer_MFM = lapply(tau, function(x) env_mfmsbmseq$MFMSBM_inference(fit_MFM$q, x))
+    invisible(infer_MFM)
   },
   RBFK = {
     fit_rbfk = noisySBM::fitNSBM(X, model = "Gauss01")
     infer_rbfk = lapply(tau, function(x) noisySBM::graphInference(X, nodeClustering = fit_rbfk[[2]]$clustering, theta = fit_rbfk[[2]]$theta, alpha = x, modelFamily = "Gauss")$A)
     invisible(infer_rbfk)
   },
-  MFMSBM_seq = {
-    fit_MFM = env_mfmsbmseq$MFMSBM_estimation(X, niterations = 300, delta = 2, xi = .5, rou = 2.5, kappa = 1, alpha = 4, beta = 7, gamma = 10, lambda = 2)
-    infer_MFM = lapply(tau, function(x) env_mfmsbmseq$MFMSBM_inference(fit_MFM$q, x))
-    invisible(infer_MFM)
+  KLLN = {
+    fit_klln = env_klln$main_noisySBM_fit(X, NIG = TRUE)
+    infer_klln = lapply(tau, function(x) env_klln$main_noisySBM_infer(fit_klln$Rho, fit_klln$theta, fit_klln$Z, X, alpha = x)$A)
+    invisible(infer_klln)
   },
   MFMSBM_sim = {
     fit_MFM = env_mfmsbmsim$MFMSBM_estimation(X, niterations = 300, delta = 2, xi = .5, rou = 2.5, kappa = 1, alpha = 4, beta = 7, gamma = 10, lambda = 2)
